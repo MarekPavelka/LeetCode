0a21ff65929a6347c6b63d66d4d7c3b3f3b52c15
Marek Pavelka <marek.pavelka@gmail.com>
2019-04-09 12:27:55 +0200

    daily commit

--- LeetCode/LongestCommonPrefix/Program.cs
+++ LeetCode/LongestCommonPrefix/Program.cs
@@ -1,27 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 //Write a function to find the longest common prefix string amongst an array of strings.
-
 //If there is no common prefix, return an empty string "".
 
 //Example 1:
-
 //Input: ["flower","flow","flight"]
 //Output: "fl"
-//Example 2:
 
+//Example 2:
 //Input: ["dog","racecar","car"]
 //Output: ""
 //Explanation: There is no common prefix among the input strings.
+
 //Note:
-
 //All given inputs are in lowercase letters a-z.
-
 
 namespace LongestCommonPrefix
 {
@@ -30,26 +23,21 @@ namespace LongestCommonPrefix
         static void Main()
         {
             Console.WriteLine("Enter words abstracted with \",\": ");
-            var stringArrayFromUserInput =
-                Console.ReadLine().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var stringArrayFromUserInput = Console.ReadLine().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
 
             var trimmedInput = ReplaceWhiteSpaces(stringArrayFromUserInput);
-
             var lengthOfUserInputs = trimmedInput.Select(l => l.Length).ToList();
 
-            var shortestInputInInputs = lengthOfUserInputs.Min();
-            var indexOfShortestInput = lengthOfUserInputs.FindIndex(l => l == shortestInputInInputs); //predicate<int>
-
+            var indexOfShortestInput = lengthOfUserInputs.FindIndex(l => l == lengthOfUserInputs.Min()); //predicate<int>
             var isEqualCounter = 0;
-            for (int sequenceCut = shortestInputInInputs; sequenceCut >= 1; sequenceCut--)
+
+            for (int sequenceCut = lengthOfUserInputs.Min(); sequenceCut >= 1; sequenceCut--)
             {
-                var cuttedString = new String(trimmedInput[indexOfShortestInput].Take(sequenceCut).ToArray());
+                var cuttedString = trimmedInput[indexOfShortestInput].Take(sequenceCut);
 
                 for (int item = 0; item < trimmedInput.Count; item++)
                 {
-                    var actualString = new String(trimmedInput[item].Take(sequenceCut).ToArray());
-
-                    if (actualString.SequenceEqual(cuttedString)) //contains
+                    if (trimmedInput[item].Take(sequenceCut).SequenceEqual(cuttedString)) //contains
                     {
                         isEqualCounter++;
                     }
@@ -61,16 +49,14 @@ namespace LongestCommonPrefix
                 }
                 else if (isEqualCounter == trimmedInput.Count)
                 {
-                    Console.WriteLine($"Longest common prefix is {cuttedString}");
+                    Console.WriteLine($"Longest common prefix is {new string(cuttedString.ToArray())}");
                     break;
                 }
             }
-
             if (isEqualCounter != trimmedInput.Count)
             {
-                Console.WriteLine($"There is no common prefix among the input strings");
+                Console.WriteLine("There is no common prefix among the input strings");
             }
-
             Console.ReadLine();
         }
 
@@ -83,6 +69,5 @@ namespace LongestCommonPrefix
             }
             return trimmedList;
         }
-
     }
 }
---
+++ LeetCode/ValidParentheses/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+//Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//An input string is valid if:
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Note that an empty string is also considered valid.
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)                         
+        {
+            Console.WriteLine("Enter parenthases: ");
+            var userInput = Console.ReadLine();
+            Console.WriteLine($"Valid parentheses = {IsValid(userInput)}");
+            Console.ReadLine();
+        }
+
+        public static bool IsValid(string s)                            // (([]){}    [ ( { ( ( ) ) } [( )] ) ]"
+        {
+            if (s == String.Empty)
+            {
+                return true;
+            }
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+            var correctPairs = "(),[],{},";
+            var leftSideIndex = s.Length / 2 - 1;
+            var rightSideIndex = s.Length / 2;
+            var actualPair = new string(new[] { s[leftSideIndex], s[rightSideIndex] });
+
+            ulong ulongLength = Convert.ToUInt64(s.Length);
+            if ((ulongLength & (ulongLength - 1)) == 0) 
+            {
+                if (correctPairs.Contains(actualPair))
+                {
+                    return IsValid(s.Remove(leftSideIndex, 2));                     
+                }
+                if (!correctPairs.Contains(actualPair) && s.Length > 2)
+                {
+                    var powerOf2Pair = new string(new[] { s[leftSideIndex - 1], s[rightSideIndex - 1] });
+                    if (correctPairs.Contains(powerOf2Pair))
+                    {
+                        return IsValid(s.Remove(leftSideIndex - 1, 2));
+                    }
+                    return false;
+                }
+
+            }
+            if (correctPairs.Contains(actualPair))
+            {
+                return IsValid(s.Remove(leftSideIndex, 2));
+            }
+            if (!correctPairs.Contains(actualPair) && s.Length > 2)
+            {
+                var powerOf2Pair = new string(new[] { s[leftSideIndex - 1], s[rightSideIndex - 1] });
+                if (correctPairs.Contains(powerOf2Pair))
+                {
+                    return IsValid(s.Remove(leftSideIndex - 1, 2));
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
+
+
+
