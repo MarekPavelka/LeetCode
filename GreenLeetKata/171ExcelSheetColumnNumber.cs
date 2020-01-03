a91e85bd9ccf4df119fcd0c68d772ddb1d5c1d2a
Marek Pavelka <marek.pavelka@gmail.com>
2020-01-03 23:17:54 +0100

    daily leet 03012020

---
+++ GreenLeetKata/171ExcelSheetColumnNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitMSTest
+{
+    /*Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+    For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+    Example 1:
+
+    Input: "A"
+    Output: 1
+    Example 2:
+
+    Input: "AB"
+    Output: 28
+    Example 3:
+
+    Input: "ZY"
+    Output: 701*/
+
+    [TestClass]
+    public class ExcelSheetColumnNumber
+    {
+        public int TitleToNumber(string s)
+        {
+            var result = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                result += (s[i] - 64) * ((int)Math.Pow(26, s.Length - i - 1)); //ASCII A == 65
+            }
+
+            return result;
+        }
+
+        [TestMethod]
+        public void WorksCorrectly()
+        {
+        }
+    }
+}
---
+++ GreenLeetKata/172FactorialTrailingZeroes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitMSTest
+{
+    /*Given an integer n, return the number of trailing zeroes in n!.
+
+    Example 1:
+
+    Input: 3
+    Output: 0
+    Explanation: 3! = 6, no trailing zero.
+    Example 2:
+
+    Input: 5
+    Output: 1
+    Explanation: 5! = 120, one trailing zero.
+    Note: Your solution should be in logarithmic time complexity.*/
+
+    [TestClass]
+    public class FactorialTrailingZeroes
+    {
+        public int TrailingZeroes(int n)
+        {
+            var result = 0;
+            while (n > 4)
+            {
+                n = n / 5;
+                result = result + n;
+            }
+            return result;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
